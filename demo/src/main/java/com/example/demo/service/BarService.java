5b0d791d58b92d531c4660e99644318a4657c2a2
Rudi Blaha Svartveit <rudsvar@gmail.com>
2023-11-20 20:51:44 +0100

    Fix missing import

--- demo/src/main/java/com/example/demo/service/BarService.java
+++ demo/src/main/java/com/example/demo/service/BarService.java
@@ -1,6 +1,9 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.MyBean;
 
 @Service
 public class BarService {
